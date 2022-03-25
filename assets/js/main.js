01ed5d406ebc67632c11b84083a9a145ca955532
VictorEnache <victor22enache@gmail.com>
2022-03-25 17:02:49 +0100

    refresh page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ const eta = document.querySelector('.eta');
 
 const genera = document.getElementById("genera");
 
+const annulla = document.getElementById("annulla");
+
 const prezzo_al_km = 0.21;
 
 const sconto_under_18 = 0.2;
@@ -26,5 +28,13 @@ genera.addEventListener('click',
     
 function() {
     price.innerHTML = costo_biglietto.toFixed(2);
+}
+)
+
+annulla.addEventListener('click', 
+    
+function() {
+    km.value = "";
+    window.location.reload();
 }
 )
