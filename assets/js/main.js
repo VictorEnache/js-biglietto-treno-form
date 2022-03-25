3bce6ebad4134790040167c37b83f35a4d848fbf
VictorEnache <victor22enache@gmail.com>
2022-03-25 17:36:55 +0100

    integrato tutto nella funzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
-document.querySelector('.carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
-
-document.querySelector('.CP').innerHTML = Math.floor(Math.random() * 10000) + 1;
-
 const km = document.querySelector('.km');
 
 const eta = document.querySelector('.eta');
@@ -20,18 +16,31 @@ const sconto_over_65 = 0.4
 let costo_biglietto = km.value * prezzo_al_km;
 
 let price = document.querySelector('.price');
- 
-if (eta.value == 'minorenne'){
-    costo_biglietto *= (1 - sconto_under_18)
-}
-else if (eta.value == "over_65"){
-    costo_biglietto *= (1 - sconto_over_65)
-}
+
+let discount = document.querySelector(".discount");
+
 
 genera.addEventListener('click', 
     
 function() {
     price.innerHTML = costo_biglietto.toFixed(2);
+    
+    document.querySelector('.carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
+
+    document.querySelector('.CP').innerHTML = Math.floor(Math.random() * 10000) + 1;
+
+    if (eta.value == 'minorenne'){
+        costo_biglietto *= (1 - sconto_under_18)
+        discount.innerHTML = "Sconto under 18 del 20%"
+    }
+    else if (eta.value == "over_65"){
+        costo_biglietto *= (1 - sconto_over_65)
+        discount.innerHTML = "Sconto over 65 del 40%"
+    }
+    
+    else{
+        discount.innerHTML = "Non sono stati applicati sconti"
+    }
 }
 )
 
